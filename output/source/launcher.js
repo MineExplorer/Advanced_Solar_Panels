3867738a2499a6954d6b51625ed186803094f9b1
MineExplorer <vladgrz40@gmail.com>
2021-07-19 22:00:48 +0100

    Deleted trash

--- output/source/launcher.js
+++
@@ -1,9 +0,0 @@
-ModAPI.addAPICallback("ICore", function(api){
-	Launch({
-		ICore: api,
-		MobEffect: Native.PotionEffect,
-		Machine: api.requireGlobal("Machine"),
-		ArmorNanoHelmet: api.requireGlobal("ArmorNanoHelmet"),
-		ArmorQuantumHelmet: api.requireGlobal("ArmorQuantumHelmet"),
-	});
-});
